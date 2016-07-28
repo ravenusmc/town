4186e7760d3caee726689530eac28177c6f83400
ravenusmc <mcuddy77@gmail.com>
2016-07-28 11:57:52 -0400

    slight modifications

--- human.py
+++ human.py
@@ -1,8 +1,9 @@
 class Human():
 
-  def __init__(self, name, age):
+  def __init__(self, name, age, gender):
     self.name = name
     self.age = age
+    self.gender = gender 
     self.boredom = 0
 
   def walk(self):
